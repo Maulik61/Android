fb08a39f98ed591d6e0b4eb7fa09b3ba674c158e
Maulik61 <maulikbaraiya60@yahoo.com>
2017-03-27 09:27:38 +0530

    Update MainActivity.java

--- LineDraw/src/com/example/linedraw/MainActivity.java
+++ LineDraw/src/com/example/linedraw/MainActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+		InternetConnectionManager internetConnection;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		internetConnection = new InternetConnectionManager(ActivityHome.this);
 	}
 }
